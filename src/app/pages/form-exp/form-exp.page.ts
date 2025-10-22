282dbd6edeef555b10e8fdd6a61203c569b00774
Beatriz Patricia Martínez López <mlbeatrice013@gmail.com>
2025-10-21 19:55:16 -0600

    archivos base html, css y ts del formulario de experiencia

--- src/app/pages/form-exp/form-exp.page.ts
+++ src/app/pages/form-exp/form-exp.page.ts
@@ -2,18 +2,35 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-form-exp',
   templateUrl: './form-exp.page.html',
   styleUrls: ['./form-exp.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonicModule, ReactiveFormsModule]
 })
 export class FormExpPage implements OnInit {
+  form = this.fb.group({
+    fecha: ['', Validators.required],
+    descripcion: ['', [Validators.required, Validators.minLength(10)]],
+    capacidad: [null, [Validators.required, Validators.min(1)]],
+    costo: [null, [Validators.required, Validators.min(0)]],
+  });
+  constructor(private fb: FormBuilder) {
+    
+  }
 
-  constructor() { }
-
+  enviar() {
+    if (this.form.valid) {
+      console.log('Formulario válido:', this.form.value);
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
   ngOnInit() {
   }
 
