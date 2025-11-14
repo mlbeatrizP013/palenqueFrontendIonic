2cafaf01d4b9e8cc58404bd816cc8165567ed068
Saul Luis <saulluislopes45418@gmail.com>
2025-11-14 05:20:58 -0600

    creacion de header

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular'; // 1. Importa IonicModule
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  standalone: true,
+  imports:[
+    IonicModule, 
+    CommonModule
+  ]
+})
+export class HeaderComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
