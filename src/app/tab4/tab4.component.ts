51e41a2ec718fb919fbe5db8126a0da72c14f92e
Juan Antonio <11.juanantonio.102@gmail.com>
2025-11-21 08:51:49 -0600

    integracion-foro

--- src/app/tab4/tab4.component.ts
+++ src/app/tab4/tab4.component.ts
@@ -1,17 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from 'src/app/components/header/header.component';
+import { IonHeader } from "@ionic/angular/standalone";
+import { IonicModule } from "@ionic/angular";
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-tab4',
   templateUrl: './tab4.component.html',
   styleUrls: ['./tab4.component.scss'],
-  imports: [HeaderComponent] 
+  imports: [HeaderComponent, IonHeader, IonicModule]
 })
 export class Tab4Component  implements OnInit {
+  apiKey = 'AIzaSyC3ws9P2fMAhK6w7W6TV9cn6bAk4RsF6Ko';
+  blogID = '5584682618555401483';
+  posts: any[] = [];
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private router : Router
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getPosts();
+  }
+
+  getPosts(){
+    const url = `https://www.googleapis.com/blogger/v3/blogs/${this.blogID}/posts?key=${this.apiKey}`;
+    this.http.get(url).subscribe((data:any)=>{
+      this.posts = data.items.map((item:any)=>{
+        return{
+          id:item.id,
+          title:item.title,
+          contentSnippet: item.content.substring(0,80) + '...',
+          fullContent: item.content
+        }
+      });
+    });
+  }
+
+  openPost(post:any){
+    this.router.navigate(['/post-detail',post.id]);
+  }
 
 }
