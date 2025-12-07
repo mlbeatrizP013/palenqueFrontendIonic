217b8895703dce0da8cfa8e6a91ce5636262a064
Saul Luis <saulluislopes45418@gmail.com>
2025-12-07 12:19:12 -0600

    .

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.palenque.app',
   appName: 'palenqueFrontendIonic',
-  webDir: 'www'
+  webDir: 'www',
+  
+  // 👇 ESTO ES LO QUE TE FALTA AGREGAR:
+  server: {
+    androidScheme: 'http',    // Obliga a la App a usar HTTP (no seguro) para coincidir con tu API
+    cleartext: true,          // Permite tráfico de texto plano (sin cifrar)
+    allowNavigation: ['*']    // Permite navegar a cualquier IP (tu computadora)
+  }
 };
 
 export default config;
